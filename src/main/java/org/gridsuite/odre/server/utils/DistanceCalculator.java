b9f048b0be52cd4c119257c3b0fb83513ef19999
etiennehomer <etiennehomer@gmail.com>
2020-05-27 12:17:33 +0200

    Migrate under parent pom for odre server (#8)
    * Migrate under parent pom for odre server
    
    Signed-off-by: Etienne <etiennehomer@gmai.com>

--- src/main/java/org/gridsuite/odre/server/utils/DistanceCalculator.java
+++ src/main/java/org/gridsuite/odre/server/utils/DistanceCalculator.java
@@ -12,9 +12,13 @@ import org.slf4j.LoggerFactory;
 /**
  * @author Chamseddine Benhamed <chamseddine.benhamed at rte-france.com>
  */
-public class DistanceCalculator {
+public final class DistanceCalculator {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DistanceCalculator.class);
+
+    private DistanceCalculator() {
+
+    }
 
     public static double distance(double lat1, double lon1, double lat2, double lon2, String unit) {
         if ((lat1 == lat2) && (lon1 == lon2)) {
