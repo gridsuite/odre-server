fa6dab3bec5d8f44b1f9b821ab148aea7c0fe8a9
ChamseddineBhd <chamseddine.benhamed@ensi-uma.tn>
2021-02-08 18:00:06 +0100

    fix distance function impl (#19)
    Signed-off-by: Jon Harper <jon.harper87@gmail.com>
    Co-authored-by: Jon Harper <jon.harper87@gmail.com>

--- src/main/java/org/gridsuite/odre/server/utils/DistanceCalculator.java
+++ src/main/java/org/gridsuite/odre/server/utils/DistanceCalculator.java
@@ -6,35 +6,31 @@
  */
 package org.gridsuite.odre.server.utils;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
  * @author Chamseddine Benhamed <chamseddine.benhamed at rte-france.com>
  */
 public final class DistanceCalculator {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(DistanceCalculator.class);
-
     private DistanceCalculator() {
 
     }
 
-    public static double distance(double lat1, double lon1, double lat2, double lon2, String unit) {
+    /**
+     * Compute an approximate distance in meters between two geographical points (latitude, longitude in degrees).
+     * The computation assumes that the earth is spherical and its radius is equal to 6378137 meters.
+     * @param the lat and lon of the two points in degrees
+     * @return the approximate distance in meters
+     */
+    public static double distance(double lat1, double lon1, double lat2, double lon2) {
+        // source : https://geodesie.ign.fr/contenu/fichiers/Distance_longitude_latitude.pdf
         if ((lat1 == lat2) && (lon1 == lon2)) {
             return 0;
         } else {
-            double theta = lon1 - lon2;
-            double dist = Math.sin(Math.toRadians(lat1)) * Math.sin(Math.toRadians(lat2)) + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.cos(Math.toRadians(theta));
+            double dL = lon1 - lon2;
+            double dist = Math.sin(Math.toRadians(lat1)) * Math.sin(Math.toRadians(lat2)) + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.cos(Math.toRadians(dL));
             dist = Math.acos(dist);
-            dist = Math.toDegrees(dist);
-            dist = dist * 60 * 1.1515;
-            if (unit.equals("M")) {
-                dist = dist * 1.609344 * 1000;
-            } else if (unit.equals("N")) {
-                dist = dist * 0.8684;
-            }
-            return dist;
+            //6 378 137 is the conventional earth radius
+            return dist * 6_378_137;
         }
     }
 }
