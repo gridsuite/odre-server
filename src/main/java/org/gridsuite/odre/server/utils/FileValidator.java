96906a1c9f5747629f1e9b1f6862b289d49c86a6
bendaamerahm <ahmed.bendaamer@rte-france.com>
2022-09-01 21:15:00 +0200

    code clean up

--- src/main/java/org/gridsuite/odre/server/utils/FileValidator.java
+++ src/main/java/org/gridsuite/odre/server/utils/FileValidator.java
@@ -30,7 +30,6 @@ public final class FileValidator {
 
     }
 
-    private static final String HEADERS_OF_FILE_HAS_CHANGED = "Invalid file, Headers of file {} has changed, header(s) not found: {}";
     private static final Logger LOGGER = LoggerFactory.getLogger(FileValidator.class);
     static final String COUNTRY_FR = "FR";
     static FileValidator instance;
@@ -71,8 +70,7 @@ public final class FileValidator {
                 return true;
             } else {
                 List<String> notFoundHeaders = SUBSTATIONS_EXPECTED_HEADERS.stream().filter(isChangedHeaders(headers)).collect(Collectors.toList());
-                String fileName = file.getOriginalFilename();
-                LOGGER.error(HEADERS_OF_FILE_HAS_CHANGED, fileName, notFoundHeaders);
+                LOGGER.error("Invalid file, Headers of file {} has changed, header(s) not found: {}", file.getOriginalFilename(), notFoundHeaders);
             }
         } catch (IOException e) {
             LOGGER.error(e.getMessage());
@@ -115,8 +113,7 @@ public final class FileValidator {
             LOGGER.error("The file {} has no equipment type : {}", file.getOriginalFilename(), typeOuvrage);
         } else {
             List<String> notFoundHeaders = SUBSTATIONS_EXPECTED_HEADERS.stream().filter(isChangedHeaders(headers)).collect(Collectors.toList());
-            String fileName = file.getOriginalFilename();
-            LOGGER.error(HEADERS_OF_FILE_HAS_CHANGED, fileName, notFoundHeaders);
+            LOGGER.error("Invalid file, Headers of file {} has changed, header(s) not found: {}", file.getOriginalFilename(), notFoundHeaders);
         }
     }
 
@@ -134,8 +131,7 @@ public final class FileValidator {
             mapResult.putIfAbsent(fileType.getValue(), new BufferedReader(new InputStreamReader(file.getInputStream(), StandardCharsets.UTF_8)));
         } else {
             List<String> notFoundHeaders = expectedHeaders.stream().filter(isChangedHeaders(headers)).collect(Collectors.toList());
-            String fileName = file.getOriginalFilename();
-            LOGGER.error(HEADERS_OF_FILE_HAS_CHANGED, fileName, notFoundHeaders);
+            LOGGER.error("Invalid file, Headers of file {} has changed, header(s) not found: {}", file.getOriginalFilename(), notFoundHeaders);
         }
     }
 
